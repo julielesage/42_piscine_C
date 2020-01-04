412fa264497e32b28d9b051cec63668512f64946
jlesage <57409607+julielesage@users.noreply.github.com>
2020-01-04 14:14:45 +0100

    Update ft_putnbr_base.c

--- C04/ex04/ft_putnbr_base.c
+++ C04/ex04/ft_putnbr_base.c
@@ -10,6 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/* • Écrire une fonction qui affiche un nombre dans le terminal dans une base donnée.
+• Ce nombre est fourni sous la forme d’un int et la base sous la forme d’une chaîne
+de caractères.
+• La base contient tous les symboles utilisables pour afficher le nombre :
+◦ 0123456789 est la base couramment utilisée pour représenter nos nombres
+décimaux ;
+◦ 01 est une base binaire ;
+◦ 0123456789ABCDEF est une base hexadecimale ;
+◦ poneyvif est une base octale.
+• La fonction doit gérer les nombres négatifs.
+• Si un paramètre contient une erreur la fonction n’affiche rien. Une erreur peut
+être :
+◦ base est vide ou est de taille 1 ;
+◦ base contient deux fois le même caractère ;
+◦ base contient les caractères + ou -.*/
+
 #include <unistd.h>
 
 void	ft_putchar(char c)
